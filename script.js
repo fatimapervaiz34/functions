958cd371d5f63686dcb2cee4bd30fdf0a51b526a
fatima pervaiz <fatimahpervaiz95@gmail.com>
2024-10-19 20:46:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function getFruits( name,prize,){
+    console.log(name)
+    console.log(prize)
+}
+function passData(){
+    var fruits=("mango" ,"apple")
+    getFruits("fruits",200 )
+}
+passData()
+
+
+
+
+// function createFruits(){
+//     let fruits=("mango" , "apple")
+//     return  fruits
+// }
+// let fruits=getfruits()
+// console.log(fruits);
+
+// createFruits()
